9dc66b07760ee4a0a8c0d1585577262f46d1629d
hue <lethihue030699@gmail.com>
2022-10-21 18:10:59 +0700

    working with db

--- taco-cloud-web/src/test/java/tacos/DesignTacoControllerTest.java
+++ taco-cloud-web/src/test/java/tacos/DesignTacoControllerTest.java
@@ -8,8 +8,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import tacos.Ingredient.Type;
-import tacos.web.DesignTacoController;
+import tacos.model.Ingredient;
+import tacos.model.Ingredient.Type;
+import tacos.controller.DesignTacoController;
+import tacos.model.Taco;
 
 import java.util.Arrays;
 import java.util.List;
